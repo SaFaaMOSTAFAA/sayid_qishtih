44508e9babc0ad2e81fd50c5d73c590cc9f41926
hossam Ameen <hosamameen948@gmail.com>
2026-06-15 20:37:55 +0300

    remove is_availabile

--- product/admin.py
+++ product/admin.py
@@ -9,8 +9,8 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'name_ar', 'category', 'price', 'image', 'is_available')
-    list_filter = ('category', 'is_available')
+    list_display = ('name', 'name_ar', 'category', 'price', 'image')
+    list_filter = ('category')
     search_fields = ('name', 'description', 'name_ar', 'description_ar')
     prepopulated_fields = {'slug': ('name',)}
     
