23eb622b42e292e6ce79dc9615f12318e578d501
Ahmad Tareq <tarekahmad8509@gmail.com>
2026-08-24 18:08:43 +0300

    update middleware

--- core/settings.py
+++ core/settings.py
@@ -54,9 +54,9 @@ INSTALLED_APPS += [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.locale.LocaleMiddleware",  # 👈 important
     "corsheaders.middleware.CorsMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
